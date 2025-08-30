c78a0e3db8fe64a30aeacfc046494facbfd659ad
w3rr0 <konradmateja65@gmail.com>
2025-08-30 13:20:22 +0200

    add Literal typing in tile.color

--- game.py
+++ game.py
@@ -1,21 +1,20 @@
 from dataclasses import dataclass
-from typing import List, Set
+from typing import List, Literal
+
+TileColor = Literal['Red', 'Blue', 'Yellow', 'Black', 'Joker']
 
 # Dataclass dla zwięzłości i czytelności
 @dataclass(frozen=True, order=True)
 class Tile:
     number: int
-    color: str
+    color: TileColor
 
     def __repr__(self):
         # Ładniejsza reprezentacja do drukowania
         return f"{self.color[0]}{self.number}"
 
-# Definicja Jokera
-JOKER = Tile(0, "Joker")
-
 
 class GameState:
-    def __init__(self, hand: Set[Tile], table: List[List[Tile]]):
+    def __init__(self, hand: List[Tile], table: List[List[Tile]]):
         self.hand = hand
         self.table = table
