08538dd6bc8e78ecdfcbb64895eead254b9c5cf5
w3rr0 <konradmateja65@gmail.com>
2025-08-31 22:14:35 +0200

    add file for training

---
+++ train.py
@@ -0,0 +1,28 @@
+import argparse
+
+from environment import RummikubEnv
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--players", type=int, default=2)
+parser.add_argument("--blocks_start", type=int, default=14)
+parser.add_argument("--blocks_range", type=int, default=13)
+
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    env = RummikubEnv(players=args.players, blocks_start=args.blocks_start, blocks_range=args.blocks_range)
+    obs, _ = env.reset()
+    done = False
+    env.render()
+
+    n_round = 1
+    print(f"--- Round {n_round} ---")
+
+    while not done:
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+        env.render()
+        done = terminated or truncated
+        if env.engine.state.current_player == env.players:
+            n_round += 1
+            print(f"------ Round {n_round} ------")
