06fbec6a203c0baf65480d577795ac74d511c233
w3rr0 <konradmateja65@gmail.com>
2025-09-17 20:02:18 +0200

    add play option (human vs ai)

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
-import argparse
+from stable_baselines3.common.vec_env import SubprocVecEnv
 from sb3_contrib import MaskablePPO
+from sb3_contrib.common.wrappers import ActionMasker
+import argparse
 import numpy as np
 import time
 import torch
-from stable_baselines3.common.vec_env import SubprocVecEnv
 
 from environment import RummikubEnv
 
@@ -12,7 +13,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--players", type=int, default=2)
 parser.add_argument("--blocks_start", type=int, default=14)
 parser.add_argument("--blocks_range", type=int, default=13)
-parser.add_argument("--mode", type=str, default="test", choices=["test", "train"])
+parser.add_argument("--mode", type=str, default="test", choices=["test", "train", "play"])
 parser.add_argument("--total_games", type=int, default=1)
 parser.add_argument("--model_path", type=str, default=None)
 parser.add_argument("--save_path", type=str, default="models/ppo_rummikub")
@@ -127,3 +128,45 @@ if __name__ == "__main__":
 
         model.save(args.save_path)
         print(f"Model saved to {args.save_path}.zip")
+
+    elif args.mode == "play":
+        env = RummikubEnv(players=args.players, blocks_start=args.blocks_start, blocks_range=args.blocks_range, version=args.engine)
+        load_model()
+
+        obs, info = env.reset()
+        done = False
+        n_round = 1
+
+        print(f"\n=== Human vs AI Game ===")
+        print(f"\n------ Round {n_round} ------")
+        env.render()
+
+        while not done:
+            player = env.engine.state.current_player
+            mask = info["action_mask"]
+
+            if player == 0:
+                print("\nYour hand:", env.engine.state.hands[player])
+                print("Possible actions:", np.where(mask)[0])
+                action = int(input("Chose actions number: "))
+                while not mask[action]:
+                    action = int(input("Incorrect action, chose again: "))
+            else:
+                if model:
+                    action, _ = model.predict(obs, action_mask=mask, deterministic=True)
+                else:
+                    valid_actions = np.where(mask)[0]
+                    action = np.random.choice(valid_actions)
+
+            obs, reward, terminated, truncated, info = env.step(action)
+
+            if player == env.players - 1 and action in {0, 1}:
+                n_round += 1
+                print(f"\n------ Round {n_round} ------")
+
+            env.render()
+            done = terminated or truncated
+
+        winner = env.engine.state.winner
+        print(f"\n===== Game end =====")
+        print(f"Winner: {'You' if winner == 0 else f'AI (Player {winner})'}")
